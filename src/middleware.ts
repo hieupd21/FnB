f8f7b76a223fed47ce342c1e91a46f4731758f5f
Híu <hiu21@Hieus-MBP.lan>
2024-11-13 21:14:57 +0700

    Use middleware to navigate url

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const privatePaths = ['/manage'];
+const publicPaths = ['/login'];
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  console.log(request.url);
+  const isAuth = Boolean(request.cookies.get('accessToken')?.value);
+  // Chưa đăng nhập thì không cho vào private paths
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  if (publicPaths.some((path) => path.startsWith(path)) && isAuth) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/manage/:path', '/login'],
+};
